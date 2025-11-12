e5017f1d21d3faa8196c5aa53ea40ddfdb72668c
Balassa Hodi <hodi.balassa@gmail.com>
2025-11-12 17:46:05 +0100

    Working in Simulation.m

--- scenario2/Simulation.m
+++ scenario2/Simulation.m
@@ -1,37 +1,57 @@
 % SCENARIO 2
+%{
+All the variables containing 'dom' are for the dominant/superior vehicle.
+All the variables containing 'sub' are for the subordinate vehicle.
+
+The superior vehicle is the one who gets into danger first, the subordinate
+is the one which helps him avoid the collusion.
+
+For easier calculations the superior vehicle for us is always in the right lane,
+and the subordinate is always at the left lane.
+%}
 
 %{
 This script runs the simulation for every timestep.
 %}
 
-clearvars -except num_sim num_good_sim;
-close all;
-% clc;
+clear;
+clear global;
+clc;
 
 % Global variables
-global OK;          % [boolean] variable for debugging. True if path was created well, false if not.
-global vehsD;       % [n-by-1 double] (n: states of the discrete state space model: [psi, v_y, y])
-global vehstate;    % [n-by-m double] (n: time steps) (m: states of av. and hv.: [x_av, y_av, psi_av, x_hv])
-global t;           % [double] (time index (the index of a given time step))
-global palya;       % [n-by-m array] (n: time steps) (m: the poses of the reference route from the RRT: [x, y, psi, length])
-global costmap;     % [vehicleCostmap] (the vehicle costmap representing the planning search space around the av.)
-global kormanyszog; % [1-by-m double] (m: the steering angles actually given to the actuator in radians in each time step)
-global sebesseg;    % [1-by-m double] (m: the actual velocity of the av. in m/s in each time step)
-global va_max;      % [double] the maximum velocity of the av. in m/s
-global all_palya;   % [n-by-1 cell] (n: the timestep that contains the palya array)
-global warnings;    % [1-by-m cell] (m: the number of warnings for later purposes)
-global emergency;   % [boolean] (true if the emergency situation happens (go straight with max deceleration)
+global steps;           % the steps of one simulation
+global dom_init_pos;    % The initial position of the dominant vehicle
+global dom_goal_pos;    % The goal position of the dominant vehicle
+global sub_init_pos;    % The initial position of the subordinate vehicle
+global sub_goal_pos;    % The goal position of the subordinate vehicle
+global dom_vmax;        % The maximum velocity of the dominant vehicle
+global sub_vmax;        % the maximum velcity of the subordinate vehicle
+global dom_vehsD;       % the states of the discrete ss model for the dominant vehicle
+global sub_vehsD;       % the states of the discrete ss model for the subordinate vehicle
+global vehstate;       % the array containing both vehicles states: [dom_x, dom_y, dom_psi, sub_x, sub_y, sub_psi]
+global dom_kormanyszog; % delta of dom. av.
+global sub_kormanyszog; % delta of sub. av.
+global dom_sebesseg;    % vx of dom. av.
+global sub_sebesseg;    % vx of sub. av.
 
-% The steps
-steps = 15;
 
 % Initialize global variables
-all_palya = cell(steps,1);
-warnings = {};
-emergency = false;
-va_max = 60;
+dom_init_pos = [1, 2.5, 0];
+dom_goal_pos = [25, 2.5, 0];
+sub_init_pos = [20, 7.5, 0];
+sub_goal_pos = [5, 7.5, 0];
+dom_vmax = 60;
+sub_vmax = 40;
 
-% Vehicles constant parameters
+
+% Simulation parameters
+Ts = 0.1;           % Sample time
+steps = ceil(((dom_goal_pos(1)-dom_init_pos(1))/(dom_vmax/3.6))/Ts);     % Calculate this value: ceil(((goalX-startX)/v_x)/Ts)
+T = 0:Ts:steps*Ts;  % The times belonging to the time indices
+current_run = 0;    % number of simulation steps in the for loop
+
+
+% Vehicles constant parameters - for both av.
 C1 = 80000;     % cornering stiffness of front tires
 C2 = 120000;    % cornering stiffness of back tires
 l1 = 2.2;       % distance between COG and front axle
@@ -39,75 +59,30 @@ l2 = 2.3;       % distance between COG and back axle
 J = 2500;       % inertia around axis z of the vehicle
 m = 1500;       % mass of the vehicle
 
-% Simulation constant parameters
-Ts = 0.1;           % Sample time
-T = 0:Ts:steps*Ts;  % The times belonging to the time indexes
-
-% State space equation
-% x_dot = A * [ddot_psi; dot_vy; vy] + B * delta;
-
-
-% Number of simulation step in the foor loop (displayed on command window)
-current_run = 0;
 
 % Initial values
-va = va_max/3.6;                 % the maximum velocity of the av. in m/s
-vh = 40/3.6;                     % human-driven vehicle velocity in m/s (for the whole simulation remains the same)
-vehsD = [0; 0; 0];               % initial condition
-vehstate(1,:) = [1, 2.5, 0, 15]; % the av. is in the begining of the route and in the middle of its lane, ...
-                                 % the hv. is at the opposite end of the opposite lane
-kormanyszog(1) = 0;              % the initial steering angle in radians
-sebesseg(1) = va;                % av. initial velocity in m/s
-
+dom_v = dom_vmax/3.6;       % dom. init vel. in m/s
+sub_v = sub_vmax/3.6;      % sub. init vel. in m/s
+dom_vehsD = [0;0;0];        % [psi_dot, vy, y]
+sub_vehsD = [0;0;0];        % [psi_dot, vy, y]
+vehstate(1,:) = [dom_init_pos, sub_init_pos]; % [dom_x, dom_y, dom_psi, sub_x, sub_y, sub_psi]
+dom_kormanyszog(1) = 0;     % in rad
+sub_kormanyszog(1) = 0;     % in rad
+dom_sebesseg(1) = dom_v;    % in m/s
+sub_sebesseg(1) = sub_v;    % in m/s
 
 
 % The simulation (loop that runs for every timestep)
 for t = 2:length(T)
-    % The number of the current simulation step (display it to cmd wdw)
-    current_run = current_run + 1
+    % The number of the current simulation step (display it to cmd wdw if needed)
+    current_run = current_run + 1;
 
-    % Generate the path that the av. should follow
-    palya = palyagen(vehstate(t-1,:));
-
-    % If there's an error during the generation of the "palya", emergency scenario is true
-    if ~OK
-        emergency = true;
-        warnings{end+1} = sprintf('(%d): Vészhelyzet! A jármű egyenesen halad maximális fékezéssel!', t-1);
-    end
-    
-
-    % Refresh the position of the hv.
-    vehstate(t,4) = vehstate(t-1,4) - Ts*vh;
-    
-    if ~emergency
-        % Optimize the steering angle of the av.
-        A = [];
-        b = [];
-        Aeq = [];
-        beq = [];
-        lb = -15*pi/180;    % the lower bound of the steering angle
-        ub=15*pi/180;       % the upper bound of the steering angle
-        % based on the function created in 'fun.m', the fmincon minimalizes the cost of that function, and returns the optimized delta
-        [delta,FVAL,EXITFLAG] = fmincon(@fun,0,A,b,Aeq,beq,lb,ub);
-        % the final steering angle (that goes to the actuator) is calculated (to smooth the steering angle)
-        korm = 0.8*delta + 0.2*kormanyszog(t-1);
-    else
-        korm = 0;
-    end
-
-    % Optimize the velocity of the vehicle
-    if emergency
-        disp('Vészhelyzet van!')
-        % maximum deceleration is applied
-        seb = max(round(sebesseg(t-1)*3.6)-3,0.01);
-    else
-        % calculate the optimal velocity of the vehicle
-        seb = sebopt(korm);
-    end
-
-
+    % JUST FOR DEBUGGING NOW
     % refresh the states based on the vehicle dynamics
-    v_x = seb/3.6;
+    % FIRST for the dominant vehicle
+    seb = dom_v;
+    korm = 0;
+    v_x = seb;
     A = [(-C1*l1^2-C2*l2^2)/(J*v_x), (-C1*l1+C2*l2)/(J*v_x), 0;
          (-C1*l1+C2*l2)/(m*v_x)-v_x, (-C1-C2)/(m*v_x), 0;
          0, 1, 0];
@@ -117,49 +92,40 @@ for t = 2:length(T)
     D = [0;0];
     sysC = ss(A,B,C,D);
     sysD = c2d(sysC,Ts);
-    
-    % refresh the global vector of states of the discrete ss system
-    vehsD = sysD.A*vehsD + sysD.B*korm;
-    
-    % calculate the elements of the state of the av. based on vehsD
-    vehstate(t,3) = vehsD(1);   % psi [rad]
-    vehstate(t,1) = vehstate(t-1,1) + v_x*Ts*cos(vehstate(t,3));    % x_av [m/s]
-    vehstate(t,2) = vehstate(t-1,2) + v_x*Ts*sin(vehstate(t,3));    % y_av [m/s]
+    % refresh the vector of states for the dominant discrete ss system
+    dom_vehsD = sysD.A*dom_vehsD + sysD.B*korm;
 
-    
-    % Store the velocity and steering angle
-    sebesseg(t) = v_x;
-    kormanyszog(t) = korm;
+    % calculate the elements of vehstate
+    vehstate(t,3) = dom_vehsD(3);
+    vehstate(t,1) = vehstate(t-1,1) + v_x*Ts*cos(vehstate(t,3));
+    vehstate(t,2) = vehstate(t-1,2) + v_x*Ts*sin(vehstate(t,3));
+
+    % SECOND for the subordinate vehicle
+    seb = sub_v;
+    korm = 0;
+    v_x = seb;
+    A = [(-C1*l1^2-C2*l2^2)/(J*v_x), (-C1*l1+C2*l2)/(J*v_x), 0;
+         (-C1*l1+C2*l2)/(m*v_x)-v_x, (-C1-C2)/(m*v_x), 0;
+         0, 1, 0];
+    B = [C1*l1/J; C1/m; 0];
+    C = [0 0 1;
+        1 0 0];
+    D = [0;0];
+    sysC = ss(A,B,C,D);
+    sysD = c2d(sysC,Ts);
+    % refresh the vector of states for the sub. discrete ss system
+    sub_vehsD = sysD.A*sub_vehsD + sysD.B*korm;
+
+    % calculate the elements of vehstate
+    vehstate(t,6) = sub_vehsD(3);
+    vehstate(t,4) = vehstate(t-1,4) - v_x*Ts*cos(vehstate(t,6));
+    vehstate(t,5) = vehstate(t-1,5) - v_x*Ts*sin(vehstate(t,6));
 end
 
-% Save the results (every variable, if needed)
-% save results;
-
-% Create figures
-% The creation of the figures are insidethe palyagen.m function
-%{
-load results;
-
-for i = 1:length(vehstate)
-    if vehstate(i,1)<25
-        figure;
-        palyagen(vehstate(i,:));
-    end;
-end;
-
 figure;
-plot([0:Ts:15*Ts],[sebesseg(1) sebesseg(2:length(sebesseg))]*3.6)
-
-figure;
-plot([0:Ts:15*Ts],[0 kormanyszog(2:length(kormanyszog))]*180/pi)
-%}
-
-% Create the figure of the velocity profile, and the steering angle over time
-% And also create the actual path of the car
-if OK
-    run plot_simit.m
-    figure;
-    plot(vehstate(:,1),vehstate(:,2), 'o-b');
-    xlim([0, 30]);
-    ylim([0,10]);
-end
+plot(vehstate(:,1),vehstate(:,2), 'o-b');
+xlim([0, 30]);
+ylim([0,10]);
+hold on;
+plot(vehstate(:,4),vehstate(:,5), 'o-r');
+hold off;
